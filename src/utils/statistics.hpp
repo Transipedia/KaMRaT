dfafbbd8d00be5fd30b346acf71c2cdee324c1c3
haoliang.xue <haoliang.xue@i2bc.paris-saclay.fr>
2020-06-05 11:32:28 +0200

    change calcRank to two-loop version

--- src/utils/statistics.hpp
+++ src/utils/statistics.hpp
@@ -20,35 +20,41 @@ inline countT GetMinInPair(countT x, countT y)
 }
 
 template <typename countT>
-inline double CalcVectMean(const std::vector<countT> &x)
+inline float CalcVectMean(const std::vector<countT> &x)
 {
-    double sum = std::accumulate(x.cbegin(), x.cend(), 0.0);
+    float sum = std::accumulate(x.cbegin(), x.cend(), 0.0);
     return (sum / x.size());
 }
 
 template <typename countT>
 inline void CalcVectRank(std::vector<float> &x_rk, const std::vector<countT> &x)
 {
-    std::map<countT, size_t> x_rec;
-    for (const auto x_i : x)
+    size_t n = x.size();
+    for (size_t i(0); i < n; ++i)
     {
-        x_rec.insert({x_i, 0}).first->second++;
-    }
-    int rk(1);
-    for (const auto &elem : x_rec)
-    {
-        x_rk.push_back((2 * rk + elem.second - 1) / 2.0);
-        rk += elem.second;
+        size_t r = 1, s = 0;
+        for (size_t j(0); j < n; ++j)
+        {
+            if (x[i] == x[j])
+            {
+                ++s;
+            }
+            else if (x[j] < x[i])
+            {
+                ++r;
+            }
+        }
+        x_rk.push_back(r + 0.5 * (s - 1));
     }
 }
 
 template <typename countT>
-inline double CalcPearsonCorrelation(const std::vector<countT> &x, const std::vector<countT> &y)
+inline float CalcPearsonCorrelation(const std::vector<countT> &x, const std::vector<countT> &y)
 {
-    double mean_x = CalcVectMean(x), mean_y = CalcVectMean(y), prod_sum(0), t1_sqsum(0), t2_sqsum(0);
+    float mean_x = CalcVectMean(x), mean_y = CalcVectMean(y), prod_sum(0), t1_sqsum(0), t2_sqsum(0);
     for (size_t i(0); i < x.size(); ++i)
     {
-        double t1 = x.at(i) - mean_x, t2 = y.at(i) - mean_y;
+        float t1 = x.at(i) - mean_x, t2 = y.at(i) - mean_y;
         prod_sum += (t1 * t2);
         t1_sqsum += (t1 * t1);
         t2_sqsum += (t2 * t2);
@@ -57,7 +63,7 @@ inline double CalcPearsonCorrelation(const std::vector<countT> &x, const std::ve
 }
 
 template <typename countT>
-inline double CalcSpearmanCorrelation(const std::vector<countT> &x, const std::vector<countT> &y)
+inline float CalcSpearmanCorrelation(const std::vector<countT> &x, const std::vector<countT> &y)
 {
     std::vector<float> x_rk, y_rk;
     CalcVectRank(x_rk, x);
@@ -66,10 +72,10 @@ inline double CalcSpearmanCorrelation(const std::vector<countT> &x, const std::v
 }
 
 template <typename countT>
-inline double CalcMeanAbsoluteContrast(const std::vector<countT> &x, const std::vector<countT> &y)
+inline float CalcMeanAbsoluteContrast(const std::vector<countT> &x, const std::vector<countT> &y)
 {
     const size_t nb_sample(x.size());
-    std::vector<double> ctrst(nb_sample);
+    std::vector<float> ctrst(nb_sample);
     for (size_t i(0); i < nb_sample; ++i)
     {
         if (x.at(i) == y.at(i)) // including the case when x[i] = y[i] = 0
@@ -78,7 +84,7 @@ inline double CalcMeanAbsoluteContrast(const std::vector<countT> &x, const std::
         }
         else
         {
-            ctrst.at(i) = fabs(static_cast<double>(x.at(i) - y.at(i))) / (x.at(i) + y.at(i));
+            ctrst.at(i) = fabs(static_cast<float>(x.at(i) - y.at(i))) / (x.at(i) + y.at(i));
         }
     }
     return (CalcVectMean(ctrst));
