5485c949a4ac33b17bdebe821694c355cad29aee
haoliang.xue <haoliang.xue@i2bc.paris-saclay.fr>
2020-09-30 15:55:57 +0200

    kamrat: accept gzipped input files

--- src/data_struct/count_tab_by_string.hpp
+++ src/data_struct/count_tab_by_string.hpp
@@ -3,20 +3,18 @@
 
 #include <vector>
 #include <string>
-#include <set>
 #include <fstream>
-#include <map>
 
 #include "count_tab_header.hpp"
 
 class CountTabByString : public CountTabHeader
 {
 public:
-    const bool IndexWithString(float &score_value, std::vector<float> &count_vect, const std::string &line_str, size_t idx_pos);
-    const void PrintFromInput(size_t row_serial, std::ifstream &index_file, const std::string &insert_at_second_col) const;
+    const bool IndexWithString(float &score_value, std::vector<float> &count_vect, const std::string &line_str, std::ofstream &index_file);
+    const void PrintFromInput(size_t row_serial, std::istream &index_file, const std::string &insert_at_second_col) const;
 
 private:
-    std::vector<size_t> input_pos_;             // indexed position for k-mer count ------ only used in onDsk
+    std::vector<size_t> index_pos_;    // indexed position for k-mer count ------ only used in onDsk
 };
 
 #endif //KAMRAT_DATASTRUCT_COUNTTAB_HPP
