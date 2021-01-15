b571e59b089bf2d6da0c2fcb743f5afa56db3bc2
haoliang.xue <haoliang.xue@i2bc.paris-saclay.fr>
2021-01-15 17:12:22 +0100

    related-scripts: submatrix-by-column.bash tested

--- related-scripts/useful_tools/split-dataset-CV.bash
+++ related-scripts/useful_tools/split-dataset-CV.bash
@@ -16,6 +16,9 @@
 #            I2BC, CEA, CNRS, Universite Paris-Saclay, Gif sur Yvette, France                              #
 ############################################################################################################
 
+NOCOLOR='\033[0m'
+RED='\033[1;31m'
+
 usage() { 
 	echo -e "Usage: $0 <Required arguments>\nRequired arguments :\n\t-s\tcomplete sample condition file\n\t-n\tfold number for cross-validation\n\t-o\toutput directory\n" 1>&2
 	exit
@@ -32,7 +35,7 @@ do
 			;;
 		o)	out_dir=$OPTARG
 			;;
-		*)	echo "Invalid option provided"
+		*)	echo -e ${RED}"ERROR: invalid option provided"${NOCOLOR}
 			usage
 			;;
 	esac
@@ -41,7 +44,7 @@ done
 if [ -z "$smp_condi_path" ] || [ -z "$nb_fold" ] || [ -z "$out_dir" ]
 then
 	echo
-	echo -e "ERROR: required argument is missing"
+	echo -e ${RED}"ERROR: required argument is missing"${NOCOLOR}
 	echo
 	usage
 fi
@@ -49,7 +52,7 @@ fi
 if [[ ! $nb_fold =~ ^[1-9][0-9]*$ ]] || [ $nb_fold -eq 1 ]
 then
 	echo
-	echo -e "ERROR: invalid fold number: "$nb_fold
+	echo -e ${RED}"ERROR: invalid fold number: "$nb_fold${NOCOLOR}
 	echo
 	exit
 fi
@@ -67,7 +70,7 @@ get_seeded_random() {
 
 if ls $out_dir/sampleshuf.tmp.*.tsv 1> /dev/null 2>&1
 then
-	echo -e "ERROR: some file named sampleshuf.tmp.*.tsv exist in the output directory\n       stop for avoiding conflict"
+	echo -e ${RED}"ERROR: some file named sampleshuf.tmp.*.tsv exist in the output directory\n       stop for avoiding conflict"${NOCOLOR}
 	exit 1
 fi
 
@@ -77,13 +80,13 @@ out_test_path=$out_dir/$(basename $smp_condi_path .tsv).test.tsv
 # Make sure that sample condtion for TRAIN does not exist
 if [ -f $out_dir/sample-condition.train.tsv ]
 then
-	echo -e "ERROR: sample-condition.train.tsv already exists in the output directory\n       stop for avoiding accidental overwriting"
+	echo -e ${RED}"ERROR: sample-condition.train.tsv already exists in the output directory\n       stop for avoiding accidental overwriting"${NOCOLOR}
 	exit 1
 fi
 # Make sure that sample condition for TEST does not exist
 if [ -f $out_dir/sample-condition.test.tsv ]
 then
-	echo -e "ERROR: sample-condition.test.tsv already exists in the output directory\n        stop for avoiding accidental overwriting"
+	echo -e ${RED}"ERROR: sample-condition.test.tsv already exists in the output directory\n        stop for avoiding accidental overwriting"${NOCOLOR}
 	exit 1
 fi
 
