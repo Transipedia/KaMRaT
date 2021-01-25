cd61bf10195e55823de0507f3e96e91742f06eb2
haoliang.xue <haoliang.xue@i2bc.paris-saclay.fr>
2021-01-25 13:03:11 +0100

    related scripts

--- related-scripts/downstream_analysis/predict_and_auc.R
+++ related-scripts/downstream_analysis/predict_and_auc.R
@@ -6,20 +6,22 @@ library(caret)
 
 cmdArgs <- commandArgs(trailingOnly = T)
 trained.mdl.path <- cmdArgs[1]
-trained.mdl.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/cv.results/b_deseq2_contigs/model.train0/trained.model.rds"
+# trained.mdl.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/cv.results/c_kamrat_contigs/model.train0/trained.model.rds"
 sig.count.path <- cmdArgs[2]
-sig.count.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/cv.results/b_deseq2_contigs/count-in-tests/contig-counts.test0.tsv"
+# sig.count.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/cv.results/c_kamrat_contigs/count-in-tests/contig-counts.test0.tsv"
 smp.info.path <- cmdArgs[3]
-smp.info.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/cv.results/a_splitted_dataset/sampleshuf.test0.tsv"
+# smp.info.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/cv.results/a_splitted_dataset/sampleshuf.test0.tsv"
 
 trained.mdl <- readRDS(trained.mdl.path)
-sig.count <- read.table(sig.count.path, header = T, row.names = 1)
+sig.count <- read.table(sig.count.path, header = T, row.names = 1) %>%
+    t() %>% 
+    data.matrix()
 smp.info <- read.table(smp.info.path, header = T)
+smp.info <- smp.info[, c("sample", "condition")]
 
-print(paste("signature matrix's order same as that in model:", all(rownames(sig.count) == rownames(trained.mdl$beta))))
+print(paste("signature matrix row names same order as that in model:", all(names(sig.count) == rownames(trained.mdl$beta))))
+print(paste("signature matrix row names same order as sample info:", all(names(sig.count) == smp.info$condition)))
 
-mdl.eval <- predict(mdl.postsel,
-                    newx = sig.count,
-                    family = "bionomial",
-                    s = mdl.postsel$lambda.1se)
-mdl.eval$auc
+mdl.eval <- assess.glmnet(trained.mdl[["trained.mdl"]], newx = sig.count, newy = smp.info$condition, 
+                          family = "binomial", s = trained.mdl[["lambda.1se"]])
+print(paste0("AUC of ", trained.mdl.path, ": ", as.numeric(mdl.eval$auc)))
--- related-scripts/downstream_analysis/signature_inference.R
+++ related-scripts/downstream_analysis/signature_inference.R
@@ -14,18 +14,16 @@ library(stringr)
 library(magrittr)
 library(Matrix)
 library(glmnet)
-library(parallel)
-library(caret)
 
 cmdArgs <- commandArgs(trailingOnly = TRUE)
 top.contig.path <- cmdArgs[1]
-top.contig.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/data/merged-diff-counts.tsv"
+# top.contig.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/cv.results/c_kamrat_contigs/kamrat-snr.train0/merged-ranked-masked-counts.top10000.tsv"
 sample.info.path <- cmdArgs[2]
-sample.info.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/data/sample-info.txt"
+# sample.info.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/cv.results/a_splitted_dataset/sampleshuf.train0.tsv"
 out.sig.path <- cmdArgs[3]
-out.sig.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/new.results/signatures.fa"
+# out.sig.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/new.results/signatures.fa"
 out.mdl.path <- cmdArgs[4]
-out.mdl.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/new.results/model.rds"
+# out.mdl.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-urine/new.results/model.rds"
 if (length(cmdArgs) == 5) {
     nf.vect.path <- cmdArgs[5]
     # nf.vect.path <- "/home/haoliang.xue/media/data/kamrat/kamrat-test/sample-nf-snr.tsv"
@@ -38,28 +36,26 @@ ExtractSignatureStb <- function(df, thres, num.runs){
     y.train <- df$condition
     feature.names <- colnames(x.train)
     model.cv.glm <- cv.glmnet(x = x.train, y = y.train, 
-                              family = "binomial", alpha = 1, type.measure = "mse")
+                              family = "binomial", alpha = 1, type.measure = "mse") # mse ? deviance ?
     StabSel <- function(i) {
         n <- nrow(x.train)
         b_sort <- sort(sample(1 : n, round(3 * n / 4)))
-        out <- glmnet(x.train[b_sort, ], y.train[b_sort],
-                      lambda = model.cv.glm$lambda.1se, family = "binomial", 
-                      alpha = 1, standardize = FALSE)
+        out <- glmnet(x = x.train[b_sort, ], y = y.train[b_sort], lambda = model.cv.glm$lambda.1se, 
+                      family = "binomial", alpha = 1, standardize = FALSE) # standardize = FALSE ? TRUE ?
         return(as.numeric(out$beta[, ncol(out$beta)] != 0))
     }
     res.cum <- lapply(1 : num.runs, FUN = StabSel) %>%
         do.call(what = rbind) %>% 
         colSums()
     prob.sel <- res.cum / num.runs
-    # plot(sort(prob.sel))
     feature.stabsel <- feature.names[prob.sel >= thres]
-    return (feature.stabsel)
+    return (list("lambda.1se" = model.cv.glm$lambda.1se, "feature.sel" = feature.stabsel))
 }
 
 print(paste0("Top contig path:", top.contig.path))
 
-sample_info <- read.table(sample.info.path, header = F)
-names(sample_info) <- c("sample", "condition")
+sample_info <- read.table(sample.info.path, header = T)
+sample_info <- sample_info[, c("sample", "condition")]
 
 top_contigs <- read.table(top.contig.path, header = T, sep = "\t")
 rownames(top_contigs) <- top_contigs$contig
@@ -79,28 +75,22 @@ feature.tab$sample <- row.names(feature.tab)
 data.tab <- merge(x = feature.tab, y = sample_info, by = "sample", all.x = TRUE)
 
 thres.lasso = 0.5 # threshold for Lasso logistic regression
-sig.contigs <- ExtractSignatureStb(data.tab, thres = thres.lasso, num.runs = 2e3)
+sel.res <- ExtractSignatureStb(data.tab, thres = thres.lasso, num.runs = 2e3)
+lambda.1se <- sel.res[["lambda.1se"]]
+contig.sel <- sel.res[["feature.sel"]]
 
 # Train model with selected signatures
-data.tab.sel <- data.tab[, c(sig.contigs, "condition")]
-ctrl <- trainControl(method = "repeatedcv",
-                     number = 10,
-                     repeats = 20,
-                     classProbs = TRUE,
-                     summaryFunction = twoClassSummary,
-                     savePredictions = TRUE)
-mdl.postsel <- train(condition ~ ., data = data.tab.sel,
-                     method = "glm",
-                     metric = "ROC",
-                     family = "binomial",
-                     preProc = c("center", "scale"),
-                     trControl = ctrl)  
+data.tab.sel <- data.tab[, c(contig.sel, "condition")]
+mdl.postsel <- glmnet(x = data.matrix(data.tab[, contig.sel]),
+                      y = data.tab$condition, type.measure = "auc", # mse ? auc ? deviance ?
+                      lambda = lambda.1se,
+                      family = "binomial", alpha = 1, standardize = TRUE)
 
 # Save model and contig fasta
-saveRDS(mdl.postsel, file = out.mdl.path)
+saveRDS(list("trained.mdl" = mdl.postsel, "lambda.1se" = lambda.1se), file = out.mdl.path)
 
-contig.names <- paste0(">contig_", seq(1 : length(sig.contigs)))
-sig.contigs.fa <- as.vector(t(cbind(contig.names, matrix(sig.contigs, length(sig.contigs), byrow = T))))
+contig.names <- paste0(">sig_contig_", seq(1 : length(contig.sel)))
+sig.contigs.fa <- as.vector(t(cbind(contig.names, matrix(contig.sel, length(contig.sel), byrow = T))))
 out.filename <- paste0(out.sig.path) %>%
     str_replace(pattern = ".tsv", replacement = ".fa")
 write.table(sig.contigs.fa, file = out.filename, 
