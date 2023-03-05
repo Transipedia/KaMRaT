9317b4c8d0755e152112c448d63cbb97b7f1909f
Haoliang Xue <hx2@sanger.ac.uk>
2023-03-05 13:39:40 +0000

    final version of summarised contig length distribution

--- validation-scripts/bench-merge/check_d4_ctglen_distribution.R
+++ validation-scripts/bench-merge/check_d4_ctglen_distribution.R
@@ -2,8 +2,7 @@ rm(list = ls())
 
 library(tidyr)
 library(ggplot2)
-library(ggrepel)
-
+library(patchwork)
 
 # work.dir <- "/data/work/I2BC/haoliang.xue/kamrat-new-res/Results/bench-merge/"
 work.dir <- "../../../revision/"
@@ -11,10 +10,10 @@ work.dir <- "../../../revision/"
 min_len <- 61
 
 pdf(paste0(work.dir, "results/check4_ctglen_distribution_summarised.pdf"),
-    width=9, height=5)
+    width=15, height=10)
+align.res <- NULL
 for (mode in c("pearson", "spearman", "mac")) {
-    align.res <- NULL
-    for (thres in seq(0.1, 1, 0.1)) {
+    for (thres in seq(0.1, 0.9, 0.2)) {
         # KaMRaT with intervention
         cat("KaMRaT", paste(mode, thres, sep = ":"), "\n")
         align.path <- paste0(work.dir, "kamrat_ctg_align/kamrat_res_err-illumina5_2-1/depth_10/ctg-aligned.",
@@ -25,21 +24,25 @@ for (mode in c("pearson", "spearman", "mac")) {
                                              align.res.x$qlen == align.res.x$length &
                                              align.res.x$qlen == align.res.x$nident &
                                              align.res.x$pident == 100,
-                                         yes = "aligned perfectly", no = "others")
+                                         yes = "yes", no = "no")
         align.res.x$threshold <- as.character(thres)
+        align.res.x$mode <- mode
         align.res <- rbind(align.res, align.res.x)
     }
-    align.res$threshold <- factor(align.res$threshold, levels = as.character(seq(1, 0, -0.1)))
-    plt <- ggplot(data = align.res) +
-        geom_bin2d(aes(x = qlen, y = threshold),
-                   binwidth = c(1, 0.1)) +
-        facet_grid(perf.align ~ .) +
-        scale_fill_gradient(trans = "log10", breaks = c(1, 1e2, 1e4, 1e6)) +
-        scale_x_continuous(breaks = c(31, 61, 100, 150, 200, 250)) +
-        ggtitle(paste0(mode)) +
-        theme_light() +
-        theme(text = element_text(size = 15, family = "sans"),
-              panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-    plot(plt)
 }
+align.res$threshold <- factor(align.res$threshold, levels = as.character(seq(0.9, 0.1, -0.2)))
+align.res$panel <- factor(paste(align.res$mode, align.res$perf.align, sep = "-"),
+                          levels = c("pearson-yes", "spearman-yes", "mac-yes",
+                                     "pearson-no", "spearman-no", "mac-no"))
+plt <- ggplot(data = align.res) +
+    geom_bin2d(aes(x = qlen, y = threshold), binwidth = c(1, 0.1)) +
+    facet_grid(panel ~ .) +
+    scale_fill_gradientn(trans = "log10", breaks = c(1, 1e2, 1e4, 1e6),
+                         colours = c("gray95", "gray50", "black")) +
+    scale_x_continuous(breaks = c(31, 61, 100, 150, 200, 250), limits = c(0, 290)) +
+    theme_bw() +
+    theme(text = element_text(size = 15, family = "sans"),
+          axis.title.x = element_blank(), strip.text = element_blank(),
+          panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+plot(plt)
 dev.off()
